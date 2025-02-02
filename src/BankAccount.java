2f7cb1792fd791e88d7200e87e7365f9182c373d
sammyCZ <161363192+sammyCZ@users.noreply.github.com>
2025-02-02 19:15:31 +0000

    Commit 1

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,14 @@
+public class BankAccount {
+    public static void main(String[] args) {
+        double balance = 2175.37; 
+        balance -= 302.50; 
+        balance += 50.03; 
+        balance /= 2; 
+        balance += 20.00;
+        --balance;
+        balance *= 2; 
+        ++balance; 
 
+        System.out.printf("Bob's new balance is: $%.2f\n" , balance);
+    }
+}
